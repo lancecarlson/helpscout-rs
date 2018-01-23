c3a84e4c4536021225f972ec300970ef641e2479
Qwuke <tristan.e.mortimer@gmail.com>
2018-01-23 16:46:17 -0500

    Added folders function for mailbox endpoint, added fields for when you use get on the customers endpoint, found undocumented fields for customers

--- tests/customers.rs
+++ tests/customers.rs
@@ -9,16 +9,40 @@ mod customers {
 
     use super::helpscout::{Client, HelpScoutError};
     use super::helpscout::api::customers::{self};
+    use super::helpscout::api::mailboxes::{self};
+
 
     #[test]
-    fn list() {
+    fn list_and_get() {
         dotenv().ok();
 
         let API_KEY = env::var("API_KEY").expect("to have API_KEY set");
 
         let mut c = Client::new(&API_KEY);
+        
         let customers = customers::list(&c).expect("Customers to be listed");
         //println!("{:?}", customers.items[0]);
         assert!(customers.items.len() > 0);
+
+        let customer = customers::get(&c, customers.items[0].id).expect("To get a customer");
+        //println!("{:?}", customer);
+        assert!(customer.item.id > 0);
+
+
     }
+
+    #[test]
+    fn list_by_mailbox(){
+        dotenv().ok();
+
+        let API_KEY = env::var("API_KEY").expect("to have API_KEY set");
+
+        let mut c = Client::new(&API_KEY);
+
+        let mailboxes = mailboxes::list(&c).expect("Grab mailboxes for testing");
+        let customers = customers::list_by_mailbox(&c,mailboxes.items[0].id).expect("Customers for the mailbox to be listed");
+        //println!("{:?}", customers.items[0]);
+        assert!(customers.items.len() > 0);
+    }
+
 }
